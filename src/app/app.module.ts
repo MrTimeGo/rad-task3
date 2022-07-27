9403808c438f12e5b074783794fd2db52da21e4c
Artem Petselia <mrtimego@ukr.net>
2022-07-27 10:30:20 +0300

    app init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BooksPageComponent } from './books-page/books-page.component';
+import { BookListComponent } from './book-list/book-list.component';
+import { BookListItemComponent } from './book-list-item/book-list-item.component';
+import { EditBookComponent } from './edit-book/edit-book.component';
+import { ViewBookComponent } from './view-book/view-book.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BooksPageComponent,
+    BookListComponent,
+    BookListItemComponent,
+    EditBookComponent,
+    ViewBookComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
